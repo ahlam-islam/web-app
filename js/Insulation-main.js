61621f7cf5c3962b1d348e74df5bf14b18e529f8
ahlam-islam <01146364974ah@gmail.com>
2020-06-25 14:55:23 +0200

    prodection and Insulation pages

---
+++ js/Insulation-main.js
@@ -0,0 +1,11 @@
+$(".close-meun").click(function () {
+    $(".menu-layout").fadeOut(500);
+  });
+  
+  $(".open-layout").click(function () {
+      $(".menu-layout").css("display", "flex");
+     $(".menu-layout").fadeIn(500);
+  })
+
+  
+
